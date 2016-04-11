5635fc3e5df19a38e538dff84ee8d9f49905fb5b
jaime sanchez <jaimesanchezcarnicer@gmail.com>
2016-04-11 17:29:28 +0200

    Cambios en el example jaime sánchez

--- session_presencial/example-jaime sanchez carnicer.php
+++ session_presencial/example-jaime sanchez carnicer.php
@@ -1,5 +1,5 @@
 <?php
-$juices = array("manzana", "naranga", "koolaid1" => "purple");
+$juices = array("apple", "naranga", "koolaid1" => "purple");
 
 class people {
     public $john = "John Smith";
@@ -21,6 +21,6 @@ echo "Don $people->john bebió un zumo de $juices[0].".PHP_EOL;
 
 // arreglar la siguiente sentencia.
 
-echo ''La $juices[0] mordida es el símbolo de la compañía de $people->steve'';
+echo ''The bitten $juices[0] is the icon of the company of $people->steve'';
 
 ?>
